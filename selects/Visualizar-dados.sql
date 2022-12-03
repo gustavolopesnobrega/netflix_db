0dc3c261aed62131a99122abb86b52d3729d74f2
gustavolopesnobrega <gustavo.lopes.nobrega@gmail.com>
2022-12-03 17:20:17 -0300

    add comentario para visualizar dados

--- selects/Visualizar-dados.sql
+++ selects/Visualizar-dados.sql
@@ -1,11 +1,14 @@
 -- 3) SELECT que visualizem todos os dados da tabela (SELECT * FROM); --
 USE db_netflix
 
-
+--visualizar todos dados da tabela usuario
 select * from dbo.tb_usuario 
 
+--visualizar todos dados da tabela video
 select * from dbo.tb_video 
 
+--visualizar todos dados da tabela avaliacao
 select * from dbo.tb_avaliacao
 
+--visualizar todos dados da tabela mensalidade
 select * from dbo.tb_mensalidade
